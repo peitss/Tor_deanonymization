f31e9caf69606f008582dc14f2c0adf40d538327
peitss <67764431+peitss@users.noreply.github.com>
2021-12-10 20:20:39 +0100

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]; then
-    echo "Usage:   script.sh <domain> <?source>"
-    echo "Example: script.sh jjay.cuny.edu lynx"
-    exit 1
-fi
+	# Creating the directory for our pcaps
+	[ -d ./pcaps/$1 ] || mkdir -pv ./pcaps/$1
+	
 
-# Create the directory if it does not already exist.
-[ -d ./pcaps/$1 ] || mkdir -pv ./pcaps/$1
+	# Creating the file name.
+	fname="$1-$(date +'%m-%d-%y_%T')"
+	
 
-# Create the file name.
-fname="$1-$(date +'%m-%d-%y_%T')"
+	if [ ! -z "$2" ]; then
+	    fname="$fname-$2"
+	    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep "$2.pcap$" | wc -l)
+	else
+	    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep ".pcap$" | wc -l)
+	fi
+	
 
-if [ ! -z "$2" ]; then
-    fname="$fname-$2"
-    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep "$2.pcap$" | wc -l)
-else
-    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep ".pcap$" | wc -l)
-fi
-
-sudo tcpdump -vv -x -X -i eth0 -A tcp and port not 22 -w ./pcaps/$1/$fname.pcap
+	sudo tcpdump -vv -x -X -i eth0 -A tcp and port not 22 -w ./pcaps/$1/$fname.pcap
